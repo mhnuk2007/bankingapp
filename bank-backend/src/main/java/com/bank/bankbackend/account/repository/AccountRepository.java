b2f32b758d801dec50b4ba2167154942bc50d027
Mohan Lal <mhnuk2007@gmail.com>
2026-01-31 16:20:50 +0500

    update account repository

--- bank-backend/src/main/java/com/bank/bankbackend/account/repository/AccountRepository.java
+++ bank-backend/src/main/java/com/bank/bankbackend/account/repository/AccountRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
@@ -22,4 +23,7 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     
     // Check existence
     boolean existsByAccountNumber(String accountNumber);
+
+    // Find by account number
+    Optional<Account> findByAccountNumber(String accountNumber);
 }
